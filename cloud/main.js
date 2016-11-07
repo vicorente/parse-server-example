acaee415eeccf2d95f5d81206c0127a1a25bae75
vicorente <victorllorente@gmail.com>
2016-11-07 21:04:56 +0100

    Update user thumbnail after updating user image

--- cloud/main.js
+++ cloud/main.js
@@ -43,3 +43,63 @@ Parse.Cloud.beforeSave("Event", function(request, response) {
     response.error(error);
   });
 });
+
+Parse.Cloud.beforeSave("_User", function(request, response) {
+  var user = request.object;
+  if (!user.get("userImage")) {
+    response.error("Users must have a profile photo.");
+    return;
+  }
+
+  if (!user.dirty("userImage")) {
+    // The profile photo isn't being modified.
+    response.success();
+    return;
+  }
+
+  Parse.Cloud.httpRequest({
+    url: user.get("userImage").url()
+  }).then(function(response) {
+    var image = new Image();
+    return image.setData(response.buffer);
+
+  }).then(function(image) {
+    // Crop the image to the smaller of width or height.
+    var size = Math.min(image.width(), image.height());
+    return image.crop({
+      left: (image.width() - size) / 2,
+      top: (image.height() - size) / 2,
+      width: size,
+      height: size
+    });
+
+  }).then(function(image) {
+    // Resize the image to 64x64.
+    return image.scale({
+      width: 64,
+      height: 64
+    });
+
+  }).then(function(image) {
+    // Make sure it's a JPEG to save disk space and bandwidth.
+    return image.setFormat("JPEG");
+
+  }).then(function(image) {
+    // Get the image data in a Buffer.
+    return image.data();
+
+  }).then(function(buffer) {
+    // Save the image into a new file.
+    var base64 = buffer.toString("base64");
+    var cropped = new Parse.File("thumbnail.jpg", { base64: base64 });
+    return cropped.save();
+
+  }).then(function(cropped) {
+    // Attach the image file to the original object.
+    user.set("userThumbnail", cropped);
+  }).then(function(result) {
+    response.success();
+  }, function(error) {
+    response.error(error);
+  });
+});
